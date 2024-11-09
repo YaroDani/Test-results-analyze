e69eb8c0279a1615bb01c5058a1b0bdbf914445c
YaroDani <sasha2284586@gmail.com>
2024-11-09 12:36:13 +0200

    Deformated bars

--- tasks_sprint2.py
+++ tasks_sprint2.py
@@ -1,34 +1,44 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df= pd.read_csv("StudentsPerformance.csv")
-print(df.info())
-'''
-temp = df.groupby(by='test preparation course')[['math score',
-                                                 'reading score', 'writing score']].mean()
-print(temp)
 
+#print(df.info())
+#temp = df.groupby(by='test preparation course')[['math score','reading score', 'writing score']].mean()
+#print(temp)
+
+
+'''
 categories = ["Math prepare", "Reading prepare","Writing prepare","Math unprepare",
-"Reading unprepare","Writing unprepare"] values = [69.69,73.89,74.82, 64.1, 66.53, 64.50]          # Список значень 
-plt.bar(categories, values)    # Метод bar створює стовпчасту діаграму; categories - підписи, 
-plt.title("Стовпчаста діаграма")  # Додає заголовок діаграми plt.xlabel("Категорії")  # 
-plt.ylabel("Значення")   # Підпис осі Y plt.show()
-'''
-
-'''
-categories = ["Math associate's degree", "Reading associate's degree", "Writing associate's degree",
-              "Math bachelor's degree", "Reading bachelor's degree", "Writing bachelor's degree",
-              "Math high school", "Reading high school", "Writing high school",
-              "Math master's degree", "Reading master's degree", "Writing master's degree",
-              "Math some college", "Reading some college", "Writing some college",
-              "Math some high school", "Reading some high school", "Writing some high school"]
-values = [67.882883, 70.927928,69.896396, 69.389831,73.000000,73.381356, 62.137755,64.704082,62.448980, 69.745763,
-          75.372881, 75.677966,67.128319, 69.460177, 68.840708, 63.497207, 66.938547, 64.888268]
-plt.bar(categories, values)    # Метод bar створює стовпчасту діаграму; categories - підписи, values - висоти стовпчиків
-plt.xticks(rotation=25)
-plt.xlabel("Категорії")  # Підпис осі X
-plt.ylabel("Значення")   # Підпис осі Y
+"Reading unprepare","Writing unprepare"]
+values_p = [69.69,73.89,74.82]
+values_unp = [64.1, 66.53, 64.50]
+plt.bar(categories[:3], values_p)
+plt.bar(categories[3:], values_unp)
+plt.title("Актуальність курсів")
+plt.ylabel("Бали")
 plt.show()
 '''
+
+categories= ["associate's degree","bachelor's degree","high school","master's degree","some college","some high school"]
+
+values_m = [67.882883,69.389831,62.137755,69.745763,67.128319,63.497207]
+values_r = [70.927928,73, 64.704082, 75.372881,69.460177,66.938547]
+values_w = [69.896396,73.381356,62.448980,75.677966,68.840708,64.888268]
+
+plt.bar(categories, values_m)
+plt.title("Математика")
+plt.ylabel("Бали")
+plt.show()
+
+plt.bar(categories, values_r,color="darkred")
+plt.title(" Читання")
+plt.ylabel("Бали")
+plt.show()
+
+plt.bar(categories, values_w, color="orange")
+plt.title("Письмо")
+plt.ylabel("Бали")
+plt.show()
 
 '''
 temp = df.groupby(by='gender')[['math score','reading score', 'writing score']].mean()
@@ -37,7 +47,7 @@ values = [63.633205,72.608108,72.467181, 68.728216,65.473029,63.311203]
 plt.bar(categories, values)
 plt.xticks(rotation=20)
 plt.title("Стовпчаста діаграма")
-plt.ylabel("Значення")
+plt.ylabel("Бали")
 plt.show()
 '''
 
@@ -69,6 +79,6 @@ values = [70.034109,71.654264,70.823256, 58.921127,64.653521,63.022535]
 plt.bar(categories, values)
 plt.xticks(rotation=20)
 plt.title("Стовпчаста діаграма")
-plt.ylabel("Значення")
+plt.ylabel("Бали")
 plt.show()
 '''
